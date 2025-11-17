27e552335ef7c13bdcd34e69b05482aa4e15d600
Harald Spieker <14868252+hcspieker@users.noreply.github.com>
2025-11-17 17:08:59 +0100

    Add version properties to ExampleClass

--- HelloWorldNuget/SimpleClassLibrary/ExampleClass.cs
+++ HelloWorldNuget/SimpleClassLibrary/ExampleClass.cs
@@ -4,6 +4,16 @@ namespace SimpleClassLibrary
 {
     public class ExampleClass
     {
-        public static string Version => Assembly.GetExecutingAssembly().GetName().Version!.ToString();
+        public static string Version => 
+            Assembly.GetExecutingAssembly().GetName().Version!.ToString();
+
+        public static string FileVersion => 
+            Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyFileVersionAttribute>()?.Version ?? "UNKOWN";
+
+        public static string InformationalVersion => 
+            Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion ?? "UNKOWN";
+
+        public static string AllVersions => 
+            $"Version: {Version}, File Version: {FileVersion}, Informational Version: {InformationalVersion}";
     }
 }
